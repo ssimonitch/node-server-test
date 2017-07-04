2b9d600e08ba200b770a7ec8cd3955b57b875356
Steven Simonitch <steven.simonitch@tastemade.com>
2017-07-03 17:40:47 -0700

    set port for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 // initialize app
 const app = express();
 // add support for partials
@@ -61,6 +62,6 @@ app.get('/bad', (req, res) => {
 });
 
 // listen on port 3000
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
